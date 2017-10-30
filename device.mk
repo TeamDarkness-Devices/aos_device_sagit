6729bda87b246d31bf485af492228d1a5012e3eb
Devi Sandeep Endluri V V <dendluri@codeaurora.org>
2017-10-30 10:24:53 +0530

    sagit: Add priv-app permissions to whitelist.
    Added priv-app to whitelist so as to provide priv app permissions.
    
    CRs-Fixed: 2131551
    Change-Id: I0f2446e1946cdd7cf8b1641a46818317acfaefdd

--- device.mk
+++ device.mk
@@ -257,6 +257,10 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Privapp Whitelist
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
+
 # Powerhint
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
